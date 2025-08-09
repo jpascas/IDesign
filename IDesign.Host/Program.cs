789d4056ff0cfaf3d8b5cffe566b0945da0e5531
Jhonatan Passalacqua <jhonatan.passalacqua@lumenalta.com>
2025-08-09 17:26:33 -0500

    further refactoring

--- IDesign.Host/Program.cs
+++ IDesign.Host/Program.cs
@@ -14,13 +14,10 @@ builder.Services.AddScoped<ICityManager, CityManager>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
