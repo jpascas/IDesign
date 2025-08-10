3a123c823ad4b1a6a5ff359c4595f527f54eed79
Jhonatan Passalacqua <jhonatan.passalacqua@lumenalta.com>
2025-08-09 19:42:16 -0500

    more fixing

--- IDesign.Host/Program.cs
+++ IDesign.Host/Program.cs
@@ -3,7 +3,16 @@ using IDesign.Host.Exceptions;
 using IDesign.Manager;
 using Microsoft.EntityFrameworkCore;
 
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration
+    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+    .AddJsonFile($"appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env}.json", optional: true)
+    .AddEnvironmentVariables()
+    .Build();
 
 // Add services
 builder.Services.AddDbContext<DesignDbContext>(options =>
@@ -14,7 +23,11 @@ builder.Services.AddManagers();
 builder.Services.AddAccesses();
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
@@ -26,11 +39,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
     app.MapOpenApi();
+    app.MapGet("/", context =>
+    {
+        context.Response.Redirect("/swagger");
+        return Task.CompletedTask;
+    });
 }
 
+app.UseExceptionHandler(options => { });
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+// create database and/or execute migrations if needed
 app.InitializeDatabase();
 
 app.Run();
